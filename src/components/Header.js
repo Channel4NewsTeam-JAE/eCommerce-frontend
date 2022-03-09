422182565c01375e5223016d4e85e54835b5417a
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-09 11:50:26 -0600

    completed basic navbar layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <nav className= 'navbar'>
-            <div className='nav-bar-logo'>
-               <a className='navbar-brand'>ECommerce</a>
-            </div>
-            <ul className='navbar-nav'>
-                <Link className='nav-link' to= "/">Home</Link>
-                <Link className='nav-link' to="/Cart">Cart</Link>
-                <Link className='nav-link' to= "/Team">Team </Link>
-            </ul>
-        </nav>
-    )
-}
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        ECommerce
+      </a>
+      <button
+        className="custom-toggler navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarsExample09"
+        aria-controls="navbarsExample09"
+        aria-expanded={!isNavCollapsed ? true : false}
+        aria-label="Toggle navigation"
+        onClick={handleNavCollapse}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarsExample09">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Cart">
+              Cart
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/Team">
+              Team{" "}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
