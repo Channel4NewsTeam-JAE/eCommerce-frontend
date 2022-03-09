1c6bf8ea2087a28158ebd3b2870424351fa66f0e
Adel Hasan <adelhasan@Adels-MacBook-Pro.local>
2022-03-09 16:01:19 -0600

    imported ProductPage into App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Search from './components/Search.';
+import Search from './components/Search';
+import ProductPage from './components/ProductPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Search />
+      <ProductPage />
       <Footer />
     </div>
   );
