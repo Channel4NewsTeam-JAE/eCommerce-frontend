92339888ea2492dd6dba491e943e01af50a6e3ca
jasonhensley1989 <jasonhensley1989@gmail.com>
2022-03-08 16:40:45 -0800

    Put button into bootstrap set input type

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Search  = () => {
+    return (
+        <div>
+            <input className='searchInput' type="text"/>
+            <button type="button" class="btn btn-primary">Search Stores</button>
+        </div>
+    )
+}
+
+
+
+
+export default Search;
