a31ad7a14cdbb69f39c49a6186350cf3f3a140c6
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-09 17:03:33 -0700

    added main to app rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Search from './components/Search.';
+import Search from './components/Search';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Search />
+      <Main />
       <Footer />
     </div>
   );
