f9d464b5493c14bf3ed2dfc5333008e9a528c602
jasonhensley1989 <jasonhensley1989@gmail.com>
2022-03-10 18:59:24 -0800

    syntax updates on img

--- src/components/TeamPage.js
+++ src/components/TeamPage.js
@@ -11,21 +11,20 @@ const Team = () => {
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                <img src="https://th.bing.com/th/id/R.a8f6826502f5b009ff4db1bbd17760d6?rik=HTzJpwZdf9a1tA&pid=ImgRaw&r=0" className="d-block w-100" alt=""/>
+                <img src="https://th.bing.com/th/id/R.a8f6826502f5b009ff4db1bbd17760d6?rik=HTzJpwZdf9a1tA&pid=ImgRaw&r=0" className="d-block w-100" alt="Ethan Averbukh's Photo"/>
                 <div className="carousel-caption d-none d-md-block">
                     <a href="#">Ethan Averbukh</a>
                     <p>Ethans contribution to the project</p>
                 </div>
                 </div>
                 <div className="carousel-item">
-                <img src="https://ca.slack-edge.com/T0351JZQ0-U02QR4J2XQW-abce7c044f36-512" className="d-block w-100" alt="..."/>
-                <div className="carousel-caption d-none d-md-block">
+                <img src="https://ca.slack-edge.com/T0351JZQ0-U02QR4J2XQW-abce7c044f36-512" className="d-block w-100" alt="Adel Hasan's Photo"/>
                 <a href="#">Adel Hasan</a>
                     <p>Adels Contribution to the project</p>
                 </div>
                 </div>
                 <div className="carousel-item">
-                <img src="https://th.bing.com/th/id/R.9641c4956c05b474164e84e85c7be589?rik=78I%2bO6PphQoOtA&pid=ImgRaw&r=0" className="d-block w-100" alt="..."/>
+                <img src="https://th.bing.com/th/id/R.9641c4956c05b474164e84e85c7be589?rik=78I%2bO6PphQoOtA&pid=ImgRaw&r=0" className="d-block w-100" alt="Jason Hensley's Photo"/>
                 <div className="carousel-caption d-none d-md-block">
                 <a href="#">Jason Hensley</a>
                     <p>Jasons Contribution the project</p>
