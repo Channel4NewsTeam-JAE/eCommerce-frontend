ba919d21392764019ae5126f613a9f4ff8a68758
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-11 15:55:34 -0700

    Fixed api url to heroku deployed url.

--- src/Api.js
+++ src/Api.js
@@ -2,16 +2,16 @@ import axios from 'axios'
 
 //Product Api Calls
 const getProductsAPICall = async () => {
-    const products = await axios.get("https://dashboard.heroku.com/apps/immense-crag-04296/products");
+    const products = await axios.get("https://immense-crag-04296.herokuapp.com/products");
     return products;
 }
 
 const getProductsBySearchAPICall = async (category) => {
-    const products = await axios.get(`https://dashboard.heroku.com/apps/immense-crag-04296/products/search/${category}`)
+    const products = await axios.get(`https://immense-crag-04296.herokuapp.com/products/search/${category}`)
     return products;
 }
 const postProductAPICall = async (product) => {
-    const productData = await axios.post("https://dashboard.heroku.com/apps/immense-crag-04296/products", {
+    const productData = await axios.post("https://immense-crag-04296.herokuapp.com/products", {
         "name": product.name,
         "price": product.price,
         "description": product.description,
@@ -21,7 +21,7 @@ const postProductAPICall = async (product) => {
     });
 }
 const putProductAPICall = async (product) => {
-    const productData = await axios.put(`https://dashboard.heroku.com/apps/immense-crag-04296/products/${product.id}`, {
+    const productData = await axios.put(`https://immense-crag-04296.herokuapp.com/products/${product.id}`, {
         "name": product.name,
         "price": product.price,
         "description": product.description,
@@ -31,55 +31,55 @@ const putProductAPICall = async (product) => {
     });
 }
 const patchProductAPICall = async (product_id, ) => {
-    const productData = await axios.patch(`https://dashboard.heroku.com/apps/immense-crag-04296/products/${product_id}`, {
+    const productData = await axios.patch(`https://immense-crag-04296.herokuapp.com/products/${product_id}`, {
 
     });
 
 }
 const deleteProductAPICall = async (product_id) => {
-    const productData = await axios.delete(`https://dashboard.heroku.com/apps/immense-crag-04296/products/${product_id}`);
+    const productData = await axios.delete(`https://immense-crag-04296.herokuapp.com/products/${product_id}`);
 }
 
 //-------------------------------------------------------------------//
 
 //Purchases API Calls
 const getPurchasesAPICall = async () => {
-    const purchases = await axios.get("https://dashboard.heroku.com/apps/immense-crag-04296/purchases");
+    const purchases = await axios.get("https://immense-crag-04296.herokuapp.com/purchases");
     return purchases;
 }
 const postPurchasesAPICall = async (purchase) => {
-    const purchaseData = await axios.post("https://dashboard.heroku.com/apps/immense-crag-04296/purchases", {
+    const purchaseData = await axios.post("https://immense-crag-04296.herokuapp.com/purchases", {
         "totalPrice": purchase.totalPrice,
         "paid": purchase.paid,
         "products": purchase.products
     });
 }
 const putPurchasesAPICall = async (purchase) => {
-    const purchaseData = await axios.put(`https://dashboard.heroku.com/apps/immense-crag-04296/purchases/${purchase.id}`, {
+    const purchaseData = await axios.put(`https://immense-crag-04296.herokuapp.com/purchases/${purchase.id}`, {
        "totalPrice": purchase.totalPrice,
        "paid": purchase.paid,
        "products": purchase.products
     })
 }
 const patchPurchaseAPICall = async (purchase, purchaseValue, propertyToEdit) => {
-    const purchaseData = await axios.patch(`https://dashboard.heroku.com/apps/immense-crag-04296/purchases/${purchase.id}`, {
+    const purchaseData = await axios.patch(`https://immense-crag-04296.herokuapp.com/purchases/${purchase.id}`, {
         propertyToEdit: purchaseValue
     })
 }
 const deletePurchasesAPICall = async (purchase) => {
-    const purchaseData = await axios.delete(`https://dashboard.heroku.com/apps/immense-crag-04296/purchases/${purchase.id}`);
+    const purchaseData = await axios.delete(`https://immense-crag-04296.herokuapp.com/purchases/${purchase.id}`);
 }
 
 //-------------------------------------------------------------------//
 //Seller API Calls
 
 const getSellersAPICall = async () => {
-    const sellers = await axios.get("https://dashboard.heroku.com/apps/immense-crag-04296/sellers");
+    const sellers = await axios.get("https://immense-crag-04296.herokuapp.com/sellers");
     return sellers
 
 }
 const postSellerAPICall = async (seller, product) => {
-    const sellerData = await axios.post("https://dashboard.heroku.com/apps/immense-crag-04296/sellers", {
+    const sellerData = await axios.post("https://immense-crag-04296.herokuapp.com/sellers", {
             "firstName": seller.firstName,
             "lastName": seller.lastName,
             "email": seller.email,
@@ -87,7 +87,7 @@ const postSellerAPICall = async (seller, product) => {
     });
 }
 const putSellerAPICall = async (seller, product) => {
-    const sellerData = await axios.put(`https://dashboard.heroku.com/apps/immense-crag-04296/sellers/${seller.id}`, {
+    const sellerData = await axios.put(`https://immense-crag-04296.herokuapp.com/sellers/${seller.id}`, {
         "firstName": seller.firstName,
         "lastName": seller.lastName,
         "email": seller.email,
@@ -96,12 +96,12 @@ const putSellerAPICall = async (seller, product) => {
  
 }
 const patchSellerAPICall = async (seller, sellerPropertyEdit, sellerPropertyValue) => {
-    const sellerData = await axios.patch(`https://dashboard.heroku.com/apps/immense-crag-04296/sellers/${seller.id}`, {
+    const sellerData = await axios.patch(`https://immense-crag-04296.herokuapp.com/sellers/${seller.id}`, {
         sellerPropertyEdit: sellerPropertyValue
     })
 }
 const deleteSellerAPICall = async (seller) => {
-    const sellerData = await axios.delete(`https://dashboard.heroku.com/apps/immense-crag-04296/sellers/${seller.id}`)
+    const sellerData = await axios.delete(`https://immense-crag-04296.herokuapp.com/sellers/${seller.id}`)
 }
 
 export {
